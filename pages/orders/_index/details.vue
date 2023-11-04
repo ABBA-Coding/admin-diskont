3d269ac753875a191d4c280cdf3aecbdeada2baf
Khasan_Ch <chulliyevhasan1405@mail.ru>
2023-11-04 17:34:29 +0500

    status pay

--- pages/orders/_index/details.vue
+++ pages/orders/_index/details.vue
@@ -160,6 +160,34 @@
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path
+                          opacity="0.3"
+                          d="M3.20001 5.91897L16.9 3.01895C17.4 2.91895 18 3.219 18.1 3.819L19.2 9.01895L3.20001 5.91897Z"
+                          fill="currentColor"
+                        ></path>
+                        <path
+                          opacity="0.3"
+                          d="M13 13.9189C13 12.2189 14.3 10.9189 16 10.9189H21C21.6 10.9189 22 11.3189 22 11.9189V15.9189C22 16.5189 21.6 16.9189 21 16.9189H16C14.3 16.9189 13 15.6189 13 13.9189ZM16 12.4189C15.2 12.4189 14.5 13.1189 14.5 13.9189C14.5 14.7189 15.2 15.4189 16 15.4189C16.8 15.4189 17.5 14.7189 17.5 13.9189C17.5 13.1189 16.8 12.4189 16 12.4189Z"
+                          fill="currentColor"
+                        ></path>
+                        <path
+                          d="M13 13.9189C13 12.2189 14.3 10.9189 16 10.9189H21V7.91895C21 6.81895 20.1 5.91895 19 5.91895H3C2.4 5.91895 2 6.31895 2 6.91895V20.9189C2 21.5189 2.4 21.9189 3 21.9189H19C20.1 21.9189 21 21.0189 21 19.9189V16.9189H16C14.3 16.9189 13 15.6189 13 13.9189Z"
+                          fill="currentColor"
+                        ></path>
+                      </svg>
+                      Статус оплаты: link</span
+                    >
+                    <p :class="order?.is_paid ? 'is_paid':'not_paid'">{{ order?.is_paid ? "Оплачен" : "Неоплачен" }}</p>
+                  </div>
+                  <div class="order-details-items">
+                    <span
+                      ><svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
                           d="M20 8H16C15.4 8 15 8.4 15 9V16H10V17C10 17.6 10.4 18 11 18H16C16 16.9 16.9 16 18 16C19.1 16 20 16.9 20 18H21C21.6 18 22 17.6 22 17V13L20 8Z"
                           fill="currentColor"
                         ></path>
@@ -256,7 +284,7 @@
                         tag_canceled:
                           currentStatus == 'canceled' && item == currentStatus,
                         tag_done: currentStatus == 'done' && item == currentStatus,
-                        tag_new: currentStatus == 'new' && item == currentStatus,
+                        'd-none': item == 'new',
                         tag_returned:
                           currentStatus == 'returned' && item == currentStatus,
                       }"
